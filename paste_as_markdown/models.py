645a029b3950322533c22e45f925e3efd6dccf17
SkipCat <cecilehuguet@laposte.net>
2018-11-18 21:44:49 +0100

    Added storage of pasted markdown

--- paste_as_markdown/models.py
+++ paste_as_markdown/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Markdown(models.Model):
+    content = models.TextField()
+    alias = models.CharField(max_length=200, blank=True, unique=True)
+
+    def __str__(self):
+        return self.content
--- paste_as_markdown/views.py
+++ paste_as_markdown/views.py
@@ -1,27 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from markdown2 import Markdown
+from django.db import IntegrityError
+import markdown2
 import uuid
 
 from .forms import MarkdownForm
-
-pastes = {}
+from .models import Markdown
 
 def index(request):
+    # error = ''
+
     if request.method == 'POST':
         form = MarkdownForm(request.POST)
         if form.is_valid():
             alias = form.cleaned_data.get('alias')
-            if alias == '' or alias in pastes:
+            if alias == '':
                 alias = uuid.uuid4()
-            pastes[str(alias)] = form.cleaned_data.get('content')
-            print(pastes)
-            return HttpResponseRedirect(f'/paste-as-markdown/{alias}')
+            try: 
+                markdown = Markdown(content=form.cleaned_data.get('content'), alias=alias)
+                markdown.save()
+                return HttpResponseRedirect(f'/paste-as-markdown/{alias}')
+            except IntegrityError:
+                error = 'This alias already exists. Please choose another one.'  
     else:
         form = MarkdownForm()
-    return render(request, 'paste_as_markdown/index.html', { 'form': form })
+    
+    return render(request, 'paste_as_markdown/index.html', {
+        'form': form,
+        'error_msg': error
+    })
 
 def show(request, alias):
     return render(request, 'paste_as_markdown/show.html', {
-        'content': Markdown().convert(str(pastes.get(alias)))
+        'content': markdown2.Markdown().convert(str(Markdown.objects.get(alias=alias)))
     })
